e2af4208b90ccec530753f71aa0114a7fb14254d
bbsec3 <winlove@live.cn>
2018-12-18 10:39:13 +0800

    fix install.py

--- install.py
+++ install.py
@@ -33,16 +33,17 @@ def install_openresty( ):
 
     exec_sys_cmd('cp -r ' + openresty_pkg.replace('.tar.gz','') + '  ' + openresty_pkg.replace('.tar.gz','_build') )
 
-    os.chdir( openresty_pkg.replace('.tar.gz','_build') + 'bundle/nginx-1.14.2' )
+    os.chdir( openresty_pkg.replace('.tar.gz','_build') + '/bundle/nginx-1.14.2' )
     exec_sys_cmd('wget https://raw.githubusercontent.com/kn007/patch/45f1417c450fc82cd470cb73a32e23085c4ba3d5/nginx.patch')
     exec_sys_cmd('wget https://raw.githubusercontent.com/kn007/patch/c59592bc1269ba666b3bb471243c5212b50fd608/nginx_auto_using_PRIORITIZE_CHACHA.patch')
     exec_sys_cmd('patch -p1 < nginx.patch')
     exec_sys_cmd('patch -p1 < nginx_auto_using_PRIORITIZE_CHACHA.patch')
 
-    os.chdir(openresty_pkg.replace('.tar.gz', '_build') + 'openssl-1.1.1a')
+    os.chdir(openresty_pkg.replace('.tar.gz', '_build') + '/openssl-1.1.1a')
     exec_sys_cmd('wget https://raw.githubusercontent.com/hakasenyang/openssl-patch/master/openssl-equal-1.1.1a.patch')
     exec_sys_cmd('patch -p1 < openssl-equal-1.1.1a.patch')
 
+    os.chdir(openresty_pkg.replace('.tar.gz', '_build'))
     exec_sys_cmd('git clone https://github.com/eustas/ngx_brotli.git')
 
     exec_sys_cmd( './configure --add-module=\'ngx_brotli\' --prefix=/opt/verynginx/openresty --user=nginx --group=nginx --with-http_v2_module --with-http_sub_module --with-http_stub_status_module --with-luajit ' +
